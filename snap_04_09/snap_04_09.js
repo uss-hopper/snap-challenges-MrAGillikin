74291ec8f5a79f605d1d536113ac6fd71840b8a9
Alistair Gillikin <agillikin1@cnm.edu>
2019-07-09 08:37:57 -0600

    Made basic functions

---
+++ snap_04_09/snap_04_09.js
@@ -0,0 +1,21 @@
+/*
+  addInt is a function to add two numbers.
+ */
+function addInt (num1, num2) {
+	console.log("Adding " + num1 + " and " + num2 + "gives: ");
+	console.log(num1 + num2);
+	return num1 + num2;
+}
+
+/*
+  concatenate is a function to concatinate two strings
+ */
+function concatenate (phrase1, phrase2) {
+	combination = phrase1.toString() + phrase2.toString();
+	console.log(combination);
+	return combination;
+}
+
+addInt(6,7);
+concatenate("Locked", " on");
+
