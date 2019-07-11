7b59264d4b4ee6866d6dfd7c8588f7bdca3c614b
Alistair Gillikin <agillikin1@cnm.edu>
2019-07-11 08:25:55 -0600

    Added user story and Diagram for 4-11

--- snap_04_09/snap_04_09.js
+++ snap_04_09/snap_04_09.js
@@ -2,8 +2,8 @@
   addInt is a function to add two numbers.
  */
 function addInt (num1, num2) {
-	console.log("Adding " + num1 + " and " + num2 + "gives: ");
-	console.log(num1 + num2);
+	//console.log("Adding " + num1 + " and " + num2 + "gives: ");
+	//console.log(num1 + num2);
 	return num1 + num2;
 }
 
@@ -12,7 +12,7 @@ function addInt (num1, num2) {
  */
 function concatenate (phrase1, phrase2) {
 	combination = phrase1.toString() + phrase2.toString();
-	console.log(combination);
+	//console.log(combination);
 	return combination;
 }
 
