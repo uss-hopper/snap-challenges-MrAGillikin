40ec23842824f74d967f7e99b9283c86b1fad83f
Alistair Gillikin <agillikin1@cnm.edu>
2019-09-03 09:38:45 -0600

    Snap 9-3

--- snap-09-03/script.js
+++ snap-09-03/script.js
@@ -1,11 +1,19 @@
 let arr = ['Bob', 'Jim', 'Matt', 'Mary', 'Suzy', ' Sarah'];
 
 
-for(i=0; i < arr.length; i++){
-	setTextValue("element"+i, arr[i]);
-}
+//for(i=0; i < arr.length; i++){
+//	setTextValue("element"+i, arr[i]);
+//}
 
-function setTextValue (identifier, value){
-	let tag = document.getElementById(identifier);
-	tag.innerText = value;
-}
+//function setTextValue (identifier, value){
+//	let tag = document.getElementById(identifier);
+//	tag.innerText = value;
+//}
+
+arr.map(name => {
+	const newName = document.createElement(p);
+
+	const newContent = document.createTextNode(name);
+	newName.appendChild(newContent);
+	document.getElementById("names").appendChild(newName)
+});
