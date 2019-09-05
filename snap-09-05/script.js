84b663d6974fa1aa0778d387b6a16eaf14e0da4c
Alistair Gillikin <agillikin1@cnm.edu>
2019-09-05 08:12:00 -0600

    Snap 9-5

---
+++ snap-09-05/script.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+const getBreeds = async() => {
+	try {
+		return await axios.get('https://dog.ceo/api/breeds/list/all')
+	} catch (error){
+		console.error(error);
+	}
+};
+
+document.getElementById("target").value = getBreeds;
